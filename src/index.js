14814d7c35a33b3d8ed456f1246f00af81b7d71f
Fajrendajs <40194093+Fajrendajs@users.noreply.github.com>
2019-03-11 12:37:10 +0100

    Kreirane komponente

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h3>h3</h3>
+      <h1>Marioplan</h1>
     </div>
   );
 }
