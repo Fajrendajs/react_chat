75adc46f0ad8926f4b2d95f12f514880df4dbe12
Fajrendajs <40194093+Fajrendajs@users.noreply.github.com>
2019-04-14 12:54:46 +0200

    styled

--- src/components/dashboard/Notifications.jsx
+++ src/components/dashboard/Notifications.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-export const Notifications = () => {
-  return (
-    <div>
-      <p>Notifications</p>
-    </div>
-  );
-};
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+const Heading = styled.h4`
+  border: 1px solid #ddd;
+  background: rebeccapurple;
+  color: white;
+  padding: 15px;
+  margin-bottom: 0;
+`;
+
+const Content = styled.div`
+  border: 1px solid gray;
+  border-top: none;
+  opacity: ${props => (props.open ? "1" : "0")};
+  max-height: ${props => (props.open ? "100%" : "0")};
+  overflow: hidden;
+  padding: ${props => (props.open ? "15px" : "0 15px")};
+  transition: all 0.3s;
+
+  p {
+    margin: 0;
+  }
+`;
+
+export class Notifications extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.toggleOpen = this.toggleOpen.bind(this);
+  }
+
+  toggleOpen() {
+    this.setState({ open: !this.state.open });
+  }
+  render() {
+    return (
+      <div>
+        <p>Notifications</p>
+        <div>
+          <Heading onClick={this.toggleOpen}>Click me to open</Heading>
+          <Content open={this.state.open}>
+            <p>Consider me opened!</p>
+          </Content>
+        </div>
+        <div>
+          <Input defaultValue="@probablyup" type="text" />
+          <Input
+            defaultValue="@geelen"
+            type="text"
+            inputColor="rebeccapurple"
+          />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/styled/input.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "react-dom";
+
+import styled from "styled-components";
+
+// Create an Input component that'll render an <input> tag with some styles
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: ${props => props.inputColor || "palevioletred"};
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+// Render a styled text input with the standard input color, and one with a custom input color
+render(
+  <div>
+    <Input defaultValue="@probablyup" type="text" />
+    <Input defaultValue="@geelen" type="text" inputColor="rebeccapurple" />
+  </div>
+);
